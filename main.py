4700846b4171c490180598f1f50a9d980d205d76
collinsadi <collinsadi20@gmail.com>
2024-04-20 13:31:50 +0100

    sdf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from meta_ai_api import MetaAI
+import uvicorn
 
 ai = MetaAI()
 app = FastAPI()
@@ -8,3 +9,6 @@ app = FastAPI()
 async def get_ai_response(prompt: str):
     response = ai.prompt(message=prompt)
     return {"message": response["message"]}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
